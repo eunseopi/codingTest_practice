7d35e36493f2b3b4d02bd46136181ed5929b855e
Hushed_Mind <117704586+eunseopi@users.noreply.github.com>
2025-03-13 14:58:49 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 7.63 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,16 @@
+function solution(arr, queries) {
+    let result = [];
+    for (const [s,e,k] of queries) {
+        let minVal = Infinity;
+        
+        for (let i=s; i<=e; i++) {
+            if(arr[i] > k && arr[i] < minVal) {
+                minVal = arr[i];
+            }
+        }
+        
+        result.push(minVal === Infinity ? -1: minVal);
+    }
+    
+    return result;
+}
