bef7251bc5cb8abf8a905f2dd74f3f52eaf8f17f
Hushed_Mind <117704586+eunseopi@users.noreply.github.com>
2025-03-14 11:22:26 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 16.81 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,8 +1,10 @@
 function solution(number) {
-    let a = number.split("");
-    let b = 0;
-    for (let i=0; i<number.length; i++){
-        b += Number(a[i])
-    }
-    return b % 9;
+    // let a = number.split("");
+    // let b = 0;
+    // for (let i=0; i<number.length; i++){
+    //     b += Number(a[i])
+    // }
+    // return b % 9;
+    
+    return [...number].reduce((sum, digit) => sum + Number(digit), 0) % 9;
 }
