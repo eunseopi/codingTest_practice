a97511508c16031f3411558b8efaf0c93504dfa6
Hushed_Mind <117704586+eunseopi@users.noreply.github.com>
2025-03-17 14:32:18 +0900

    [level 0] Title: 배열 만들기 5, Time: 2.29 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,15 +1,20 @@
 function solution(intStrs, k, s, l) {
-    let sliceStrs = [];
-    for(let i=0; i<intStrs.length; i++){
-        sliceStrs.push(intStrs[i].slice(s,s+l));
-    }
+//     let sliceStrs = [];
+//     for(let i=0; i<intStrs.length; i++){
+//         sliceStrs.push(intStrs[i].slice(s,s+l));
+//     }
     
-    let numArr = [...sliceStrs].map((item) => Number(item));
-    let res =[];
-    for(let i=0; i<numArr.length; i++) {
-        if(numArr[i] > k){
-            res.push(numArr[i])
-        }
-    }
-    return res;
+//     let numArr = [...sliceStrs].map((item) => Number(item));
+//     let res =[];
+//     for(let i=0; i<numArr.length; i++) {
+//         if(numArr[i] > k){
+//             res.push(numArr[i])
+//         }
+//     }
+//     return res;
+    // 더 나은 코드
+    
+    return intStrs
+        .map(str => Number(str.slice(s, s+l)))
+        .filter(num => num > k)
 }
