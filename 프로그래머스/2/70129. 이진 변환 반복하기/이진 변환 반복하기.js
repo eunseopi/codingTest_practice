77915ed298904fb423338ce080e510c75b6d5f8a
Hushed_Mind <117704586+eunseopi@users.noreply.github.com>
2025-03-20 14:58:51 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.25 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let transCnt = 0;
+    let removeCnt = 0;
+    
+    while(s !== "1") {
+        let newS = s.replace(/0/g, "");
+        
+        removeCnt += s.length - newS.length;
+        
+        s = newS.length.toString(2);
+        transCnt++;
+    }
+    
+    return [transCnt, removeCnt];
+}
+    
