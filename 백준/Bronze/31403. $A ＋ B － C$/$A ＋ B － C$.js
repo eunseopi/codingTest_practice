e9be0cb22b3dc8e01c241166fb3b68f7f69fdbbe
Hushed_Mind <117704586+eunseopi@users.noreply.github.com>
2025-03-26 16:30:33 +0900

    [Bronze IV] Title: $A + B - C$, Time: 96 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.js
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.js
@@ -2,16 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 // const input = fs.readFileSync("dev/stdin").toString().trim().split(" ").map(Number);
 
-const num = input.slice(0).map(Number);
+const [a, b, c] = input.map(Number);
 
-let a = [...num].map(Number);
-let b = [...num].map(String);
-
-for (let i = 0; i < 1; i++) {
-  if (typeof a[i] === "number") {
-    console.log(a[0] + a[1] - a[2]);
-  }
-  if (typeof b[i] === "string") {
-    console.log(b[0] + b[1] - b[2]);
-  }
-}
+console.log(a + b - c);
+console.log(a.toString() + b - c);
