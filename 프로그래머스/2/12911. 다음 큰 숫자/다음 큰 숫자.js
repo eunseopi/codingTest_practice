9f4b50b9111785d3fafeb623a38eb98034688e37
Hushed_Mind <117704586+eunseopi@users.noreply.github.com>
2025-03-31 15:10:11 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,13 +1,10 @@
 function solution(n) {
-    // 1의 개수를 세는 함수
-    const countOnes = (x) => x.toString(2).split('1').length - 1;
-
-    // n의 이진수에서 1의 개수
-    const originalCount = countOnes(n);
-
-    // n보다 큰 숫자를 하나씩 증가시키며 1의 개수가 같은지 확인
-    while (countOnes(++n) !== originalCount) {}
-
-    // 조건을 만족하는 가장 작은 숫자를 반환
+    const a = (x) => x.toString(2).split('1').length - 1;
+    
+    const b = a(n);
+    
+    while(a(++n) !== b) {}
+    
     return n;
 }
+
