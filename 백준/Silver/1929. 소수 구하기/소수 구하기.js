23077b8b492a966fc4d507eab2d131f3ecd243f4
Hushed_Mind <117704586+eunseopi@users.noreply.github.com>
2025-04-16 08:43:03 +0900

    [Silver III] Title: 소수 구하기, Time: 2772 ms, Memory: 17004 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+for (let i = N; i <= M; i++) {
+  if (isPrime(i)) console.log(i);
+}
